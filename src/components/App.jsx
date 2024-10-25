77feacafec9ea514deb089b27e46d8a95779c220
Oleksandr-Kalnaus <oleksandrkalnaus@gmail.com>
2024-10-25 23:17:52 +0100

    The case when I began to read what to do

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,14 @@
-import { useDispatch, useSelector } from "react-redux";
-import {
-  addContact,
-  deleteContact,
-  selectContacts,
-} from "../redux/contactsSlice";
-import { selectNameFilter } from "../redux/filtersSlice";
 import ContactForm from "./ContactForm/ContactForm";
 import SearchBox from "./SearchBox/SearchBox";
 import ContactList from "./ContactList/ContactList";
-import "modern-normalize";
 
 export default function App() {
-  const contacts = useSelector(selectContacts);
-  const searchValue = useSelector(selectNameFilter);
-  const dispatch = useDispatch();
-
-  const addContactAction = (newContact) => {
-    dispatch(addContact(newContact));
-  };
-
-  const deleteContactAction = (contactId) => {
-    dispatch(deleteContact(contactId));
-  };
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(searchValue.toLowerCase().trim())
-  );
-
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm onAddContact={addContactAction} />
+      <ContactForm />
       <SearchBox />
-      <ContactList
-        contacts={filteredContacts}
-        onDeleteContact={deleteContactAction}
-      />
+      <ContactList />
     </div>
   );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,26 +1,35 @@
-import PropTypes from "prop-types";
+import { useSelector, useDispatch } from "react-redux";
 import Contact from "../Contact/Contact";
+import { deleteContact, selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
 import styles from "./ContactList.module.css";
 
-const ContactList = ({ contacts, onDeleteContact }) => {
+const ContactList = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+  const searchValue = useSelector(selectNameFilter);
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(searchValue.toLowerCase().trim())
+  );
+
+  const handleDeleteContact = (contactId) => {
+    dispatch(deleteContact(contactId));
+  };
+
   return (
     <ul className={styles.contactList}>
-      {contacts.map((contact) => (
+      {filteredContacts.map((contact) => (
         <li key={contact.id} className={styles.contactItem}>
           <Contact
             name={contact.name}
             number={contact.number}
-            onDelete={() => onDeleteContact(contact.id)}
+            onDelete={() => handleDeleteContact(contact.id)}
           />
         </li>
       ))}
     </ul>
   );
-};
-
-ContactList.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
 };
 
 export default ContactList;
